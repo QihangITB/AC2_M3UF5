bbbdc1087c4040c05000347d38e9bb8c42624495
Qihang <qihang.li.7e7@itb.cat>
2024-04-07 00:25:30 +0200

    first commit

--- AC2_M3UF5/Program.cs
+++ AC2_M3UF5/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace AC2_M3UF5
+{
+    class Program
+    {
+        static void Main()
+        {
+            string csvPath = "../../../files/Consum_d_aigua_a_Catalunya_per_comarques_20240402.csv";
+            string xmlPath = "../../../files/WaterConsumByRegions.xml";
+
+            List<Region> regions = Helper.ReadCSVFile(csvPath);
+
+            foreach (Region region in regions)
+            {
+                if (File.Exists(xmlPath))
+                {
+                    Helper.AddRegionToXMLFileWithLINQ(xmlPath, region);
+                }
+                else
+                {
+                    Helper.CreateXMLFileWithLINQ(xmlPath, region);
+                }
+            }
+        }
+    }
+}
---
+++ AC2_M3UF5/Region.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper.Configuration.Attributes;
+
+namespace AC2_M3UF5
+{
+    public class Region
+    {
+        private const int DefaultIntValue = 0;
+        private const string DefaultStringValue = "";
+
+        [Name("Any")]
+        public int Year { get; set; }
+
+        [Name("Codi comarca")]
+        public int Code { get; set; }
+
+        [Name("Comarca")]
+        public string? Name { get; set; }
+
+        [Name("Població")]
+        public int Population { get; set; }
+
+        [Name("Domèstic xarxa")]
+        public int DomesticConsum { get; set; }
+
+        [Name("Activitats econòmiques i fonts pròpies")]
+        public int EconomyConsum { get; set; }
+
+        [Name("Total")]
+        public int TotalConsum { get; set; }
+
+        [Name("Consum domèstic per càpita")]
+        public float ConsumCapita { get; set; }
+
+        public Region(int year, int code, string name, int population, int domesticConsum, int economyConsum, int totalConsum, float consumPerCapita)
+        {
+            Year = year;
+            Code = code;
+            Name = name;
+            Population = population;
+            DomesticConsum = domesticConsum;
+            EconomyConsum = economyConsum;
+            TotalConsum = totalConsum;
+            ConsumCapita = consumPerCapita;
+        }
+
+        public Region() : this(DefaultIntValue, DefaultIntValue, DefaultStringValue, DefaultIntValue, DefaultIntValue, DefaultIntValue, DefaultIntValue, DefaultIntValue)
+        { }
+        public override string ToString()
+        {
+            return $"Year: {Year}, Code: {Code}, Name: {Name}, Population: {Population}, DomesticConsum: {DomesticConsum}, EconomyConsum: {EconomyConsum}, TotalConsum: {TotalConsum}, ConsumPerCapita: {ConsumCapita}";
+        }
+    }
+}
